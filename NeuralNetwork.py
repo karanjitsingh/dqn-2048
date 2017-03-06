ee23142803c16b081a522a04f5de3db2489e0ffe
Karan Jit Singh <karan1866@gmail.com>
2017-03-06 12:49:07 +0530

    incomplete commit

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,26 +1,26 @@
 import numpy as np
 import ActivationFunctions
+import Game
 
 
 class NeuralNetwork:
-
 	def __init__(self, layers, afn=ActivationFunctions.sigmoid):
 		self.layers = layers
 		self.network = []
 		self.biases = np.random.rand(len(layers))
 		self.aFn = afn
 
-		# Weights for inputs or layer 0
+		# Weights for input layer (layer 0)
 		self.network.append(np.random.rand(layers[0], layers[0]))
 
-		for i in range(len(layers)-1):
+		for i in range(len(layers) - 1):
 			singlelayer = np.array([])
 			for x in range(layers[i]):
 				rand = np.random.rand(layers[i + 1])
 				singlelayer = np.vstack([singlelayer, rand]) if singlelayer.size else np.array([rand])
 			self.network.append(np.transpose(singlelayer))
 
-	def print_network(self,biases=True,layers=True):
+	def print_network(self, biases=True, layers=True):
 		print "Layers ", self.layers, "\n"
 		if biases:
 			print "Biases"
@@ -32,19 +32,59 @@ class NeuralNetwork:
 
 	# Feedforward propagation
 	def propagate(self, input):
-		input = np.array(input)
+		inputmatrix = np.array(input)
 		for i in range(len(self.layers)):
 			output = np.array([])
 			for x in range(self.layers[i]):
-				output = np.append(output, self.aFn(self.biases[i] + np.dot(input, self.network[i][x])))
-			input = output
+				output = np.append(output, self.aFn(self.biases[i] + np.dot(inputmatrix, self.network[i][x])))
+			inputmatrix = output
+
 		return output
 
+	def reward(self, input, confidence, output, move_score, valid):
+		if valid:
+			return move_score
+		else:
+			return -10
 
+	def train(self, game, verbose=False, log=False):
+
+		halt = False
+		i = 0
+
+		if verbose:
+			print "Starting simulation..."
+			print "i: ", i
+			print game.printgrid(), "\n"
+
+		while not halt and i < 10:
+			i += 1
+			input = game.grid_to_input()
+
+			confidence = self.propagate(input).tolist()
+			move_score = game.score
+
+			valid, full, halt, score = game.acceptinput(direction=confidence)
+
+			move_score = score - move_score
+			output = game.grid_to_input()
+
+			reward = self.reward(input, confidence, output, move_score, valid)
+
+			''' Discounted Reward '''
+
+
+			if verbose:
+				print "i:", i
+				game.printgrid()
+				print "Score: ", game.score, "\n"
+
+		if verbose:
+			print "Final Network:"
+			self.print_network()
 
 # layers = [16,16,16,16,16,4]
 # nn = NeuralNetwork(layers)
 # nn.print_network(layers=False)
 #
 # print nn.propagate([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
-
--- Simulator.py
+++ Simulator.py
@@ -1,19 +1,22 @@
 from Game import Game
 from NeuralNetwork import NeuralNetwork
 
-game = Game(4)
-nn = NeuralNetwork([16, 16, 4])
+game = Game(2)
+nn = NeuralNetwork([4, 4, 4, 4])
 nn.print_network(layers=False)
 
-game.printgrid()
-print "l"
+# game.printgrid()
+# print ""
+#
+# while 1:
+#
+# 	confidence = nn.propagate(game.grid_to_input()).tolist()
+# 	valid, full, halt, score = game.acceptinput(direction=confidence)
+# 	game.printgrid()
+# 	print "Score: ", score, "\n"
+#
+# 	if halt or not valid:
+# 		break
 
-while 1:
 
-	confidence = nn.propagate(game.grid_to_input()).tolist()
-	valid, full, halt, score = game.acceptinput(direction=confidence)
-	game.printgrid()
-	print "Score: ", score, "\n"
-
-	if halt or not valid:
-		break;
+nn.train(game, verbose=True)
