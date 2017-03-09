587010f569f88faf7524238ecdd5559675038484
Karan Jit Singh <karan1866@gmail.com>
2017-03-09 22:31:10 +0530

    fixed state object referencing 	state parameter passed to the get_next_state function was being modified implicitly

--- Game.py
+++ Game.py
@@ -1,157 +1,163 @@
 import random
+import copy
 
 
-def getkey():
-	while 1:
-		k = str(raw_input("Next: "))
-		if k == 'w' or k == 'W':
-			return getdirection('up')
-		elif k == 'a' or k == 'A':
-			return getdirection('left')
-		elif k == 's' or k == 'S':
-			return getdirection('down')
-		elif k == 'd' or k == 'D':
-			return getdirection('right')
+class State(object):
+	def __init__(self, grid, score, halt=False, full=False, valid=True):
+		self.grid = grid
+		self.score = score
+		self.halt = halt
+		self.full = full
+		self.valid = valid
+
+	def printstate(self):
+		grid = self.grid
+		for i in range(len(grid)):
+			print grid[i]
+		print (self.score, self.halt, self.full, self.valid)
 
 
-def getdirection(x):
-	directions = {
+class Game(object):
+
+	@staticmethod
+	def getkey():
+		while 1:
+			k = str(raw_input("Next: "))
+			if k == 'w' or k == 'W':
+				return Game.getdirection('up')
+			elif k == 'a' or k == 'A':
+				return Game.getdirection('left')
+			elif k == 's' or k == 'S':
+				return Game.getdirection('down')
+			elif k == 'd' or k == 'D':
+				return Game.getdirection('right')
+
+	@staticmethod
+	def getdirection(x):
+		directions = {
 			'up': [0, -1],
 			'down': [0, 1],
 			'right': [1, 0],
 			'left': [-1, 0]
 		}
-	if isinstance(x, str):
-		return directions[x]
-	elif isinstance(x, int):
-		return directions.values()[x]
-	elif isinstance(x, list):
-		return directions.values()[x.index(max(x))]
+		if isinstance(x, str):
+			return directions[x]
+		elif isinstance(x, int):
+			return directions.values()[x]
+		elif isinstance(x, list):
+			return directions.values()[x.index(max(x))]
 
-
-class Game:
-
-	def __init__(self, size, grid=[]):
-		self.grid = grid
-		self.score = 0
-		if not grid:
-			self.emptygrid(size)
-
-	def emptygrid(self, size, value=0):
-		for i in range(size):
-			self.grid.append([value] * size)
-
-		self.add_random()
-		self.add_random()
-
-	def add_random(self):
-		indices = []
-		n = len(self.grid)
-		for y in range(n):
-			for x in range(n):
-				if self.grid[y][x] == 0:
-					indices.append((x, y))
-
-		if len(indices) != 0:
-			r = indices[random.randint(0, len(indices) - 1)]
-			value = random.randint(1, 2) * 2
-			self.grid[r[1]][r[0]] = value
-
-	def acceptinput(self, direction=None, verbose=False):
-		if direction:
-			direction = getdirection(direction)
-		else:
-			direction = getkey()
-
-		halt, valid, full = self.nextmove(direction)
-
-		if verbose:
-			self.printgrid()
-			print "Valid? ", valid, "\tHalt? ", halt
-		return valid, full, halt, self.score
-
-	def movelogic(self, direction):
-		n = len(self.grid)
+	@staticmethod
+	def get_next_state(state, direction):
+		grid = copy.copy(state.grid)
+		n = len(grid)
+		score = 0
 
 		if direction[0] != 0:
 			d = direction[0]
 		elif direction[1] != 0:
 			d = direction[1]
-			self.grid = map(list, zip(*self.grid))
+			grid = map(list, zip(*grid))
 
 		valid = False
 		full = True
 
 		for y in range(n):
-
-			r = row = self.grid[y]
+			r = row = grid[y]
 			r = filter(lambda a: a != 0, r)
-
 			if d == 1:
 				# Reverse
 				r = r[::-1]
 
 			x = 0
-
 			while x < len(r) - 1:
 				# Merge
 				if r[x] == r[x + 1]:
-					self.score += r[x]*2
+					score += r[x]*2
 					r[x] = reduce(lambda x, y: x + y, r[x:x + 2])
 					r[x + 1] = 0
 					x += 1
 				x += 1
 
 			r = filter(lambda a: a != 0, r)
-
 			if d == 1:
 				# Reverse
 				r = r[::-1]
-
 			zeroes = [0] * (n - len(r))
 			if d == 1:
-				self.grid[y] = zeroes + r
+				grid[y] = zeroes + r
 			elif d == -1:
-				self.grid[y] = r + zeroes
-
-			if row != self.grid[y]:
+				grid[y] = r + zeroes
+			if row != grid[y]:
 				valid = True
-				if len(filter(lambda a: a == 0, self.grid[y])):
+				if len(filter(lambda a: a == 0, grid[y])):
 					full = False
 
 		if direction[1] != 0:
-			self.grid = map(list, zip(*self.grid))
-
-		return full, valid
-
-	def nextmove(self, direction):
-
-		n = len(self.grid)
-		full, valid = self.movelogic(direction)
-
-		if not full and valid:
-			self.add_random()
+			grid = map(list, zip(*grid))
 
 		halt = False
 		if full:  # Check for game end
 			halt = True
 			for i in range(n):
 				for j in range(n - 1):
-					if self.grid[j][i] == self.grid[j + 1][i] or self.grid[i][j] == self.grid[i][j + 1]:
+					if grid[j][i] == grid[j + 1][i] or grid[i][j] == grid[i][j + 1]:
 						halt = False
 						break
 
-		return halt, valid, full
+		if not full and valid:
+			grid = Game.add_random(grid)
+
+		return State(grid, state.score+score, halt, full, valid)
+
+	@staticmethod
+	def add_random(grid):
+		indices = []
+		n = len(grid)
+		for y in range(n):
+			for x in range(n):
+				if grid[y][x] == 0:
+					indices.append((x, y))
+
+		if len(indices) != 0:
+			r = indices[random.randint(0, len(indices) - 1)]
+			value = random.randint(1, 2) * 2
+			grid[r[1]][r[0]] = value
+
+		return grid
+
+	def __init__(self, size, grid=[]):
+		self.grid = grid
+		if not grid:
+			self.emptygrid(size)
+		self.initState = State(grid, 0)
+		self.currState = self.initState
+
+	def emptygrid(self, size, value=0):
+		for i in range(size):
+			self.grid.append([value] * size)
+
+		self.grid = Game.add_random(Game.add_random(self.grid))
+
+	def transition(self, direction=None):
+		if direction is not None:
+			direction = Game.getdirection(direction)
+		else:
+			direction = Game.getkey()
+		self.currState = Game.get_next_state(self.currState, direction)
+		return self.currState
 
 	def grid_to_input(self):
 		arr = []
-		for i in range(len(self.grid)):
-			arr = arr + self.grid[i]
+		grid = self.currState.grid
+		for i in range(len(grid)):
+			arr = arr + grid[i]
 		return arr
 
 	def printgrid(self):
-		for i in range(len(self.grid)):
-			print self.grid[i]
+		grid = self.currState.grid
+		for i in range(len(grid)):
+			print grid[i]
 
 
 # default = [
@@ -165,5 +171,6 @@ class Game:
 # game.printgrid()
 #
 # while 1:
-# 	print game.acceptinput()
+# 	state = game.transition()
 # 	game.printgrid()
+# 	print state.valid, state.halt, state.full
