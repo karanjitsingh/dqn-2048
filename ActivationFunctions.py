6a87a1b98a10103e2d5002bac7bc8dfb8403f15c
Karan Jit Singh <karan1866@gmail.com>
2017-03-28 14:45:38 +0530

    IT HAS BEGUN TO LEARN switched to numpy.exp from math.exp completed weight updation

--- ActivationFunctions.py
+++ ActivationFunctions.py
@@ -1,5 +1,4 @@
-from math import *
-
+from numpy import exp
 
 # Sigmoid function
 class Sigmoid(float or list):
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -46,7 +46,8 @@ class NeuralNetwork:
 		if not tostate.valid:
 			return -1
 		elif tostate.score - fromstate.score > 0:
-			return math.log(tostate.score - fromstate.score, 2)
+			return 1
+			# return math.log(tostate.score - fromstate.score, 2)
 		return 0
 
 	def print_network(self, biases=True, layers=True):
@@ -69,7 +70,7 @@ class NeuralNetwork:
 		for i in range(len(self.layers)):
 			output = np.array([])
 			for x in range(self.layers[i]):
-				output = np.append(output, self.aFn(self.biases[i] + np.dot(inputmatrix, self.network[i][x])))
+				output = np.append(output, self.aFn(np.dot(inputmatrix, self.network[i][x])))
 			inputmatrix = output
 
 			if returnActivations:
@@ -84,8 +85,7 @@ class NeuralNetwork:
 		activation = self.propagate(game.grid_to_input(), True)
 		qset = activation[-1].tolist()
 
-		qmax = (max(qset), qset.index(max(qset)))
-		td_error = reward + gamma * qmax[0] - qsel[0]
+		td_error = reward + gamma * np.array(qset) - qsel[0]
 
 		# Calculate deltas
 		delta = []
@@ -101,19 +101,20 @@ class NeuralNetwork:
 		input = self.aFn(input)
 		activation.insert(0, np.array(input))
 
+		# Calculate eligibility traces
 		for i in range(len(delta)-1, -1, -1):
 			activation_3d = np.array([np.diag([activation[i][k]] * delta[i][0].size) for k in range(activation[i].size)])
-			e_trace[i] = np.array([delta[i] * activation[i][k] for k in range(activation[i].size)])
+			e_trace[i] += e_scale * np.array([delta[i] * activation[i][k] for k in range(activation[i].size)])
 
-		print e_trace[0][0][0]
+		# Update weights
+		for i in range(len(self.network)):
+			del_weights = np.zeros(self.network[i].shape)
+			for k in range(e_trace[i].shape[0]):
+				for j in range(e_trace[i].shape[1]):
+					del_weights[j][k] = np.matmul(e_trace[i][k][j], td_error)
+			self.network[i] += del_weights
 
-	def train(self, game, verbose=False, log=False, total=1):
-		# Create empty e-trace
-		e_trace = []
-		output_neurons = self.layers[-1]
-		for i in range(len(self.layers)):
-			e_trace.append(np.zeros(self.network[i].shape[::-1] + (output_neurons,)))
-
+	def play(self, game, verbose=False, max=1000):
 		halt = False
 		i = 0
 		if verbose:
@@ -124,13 +125,13 @@ class NeuralNetwork:
 		state = game.currState
 		qset = self.propagate(game.grid_to_input()).tolist()
 
-		while not halt and i <= total:
+		while not halt and i < total:
 			i += 1
 
-			if random.random() < epsilon(i/total):
-				index = random.randint(0, 3)		# Choose random action
+			if random.random() < epsilon(i / total):
+				index = random.randint(0, 3)  # Choose random action
 			else:
-				index = qset.index(max(qset))		# Choose policy action
+				index = qset.index(max(qset))  # Choose policy action
 
 			qsel = (qset[index], index)
 
@@ -153,7 +154,59 @@ class NeuralNetwork:
 			# 	print "Final Network:"
 			# 	self.print_network()
 
+
+	def train(self, game, verbose=False, log=False, total=1000):
+		# Create empty e-trace
+		e_trace = []
+		output_neurons = self.layers[-1]
+
+		# e_ijk = e[k][j][i]
+		for i in range(len(self.layers)):
+			e_trace.append(np.zeros(self.network[i].shape[::-1] + (output_neurons,)))
+
+		halt = False
+		i = 0
+		if verbose:
+			print "Starting simulation..."
+			print "i: ", i
+			print game.printgrid(), "\n"
+
+		state = game.currState
+		qset = self.propagate(game.grid_to_input()).tolist()
+
+		while not halt and i < total:
+			i += 1
+
+			if random.random() < epsilon(i/total):
+				index = random.randint(0, 3)		# Choose random action
+			else:
+				index = qset.index(max(qset))		# Choose policy action
+
+			qsel = (qset[index], index)
+
+			input = game.grid_to_input()
+
+			next_state = game.transition(direction=qsel[1])
+			reward = NeuralNetwork.reward(state, next_state)
+			state = game.currState
+			halt = state.halt
+
+			# TD Learning
+			self.tdlearn(qsel, reward, e_trace, game, input)
+
+			if verbose:
+				print "i:", i
+				game.printgrid()
+				print "Reward: ", reward
+				print "Score: ", game.currState.score
+				print ""
+
+			# if verbose:
+			# 	print "Final Network:"
+			# 	self.print_network()
+
 game = Game(4)
 nn = NeuralNetwork([16, 16, 4])
 nn.train(game, verbose=True)
+
 
